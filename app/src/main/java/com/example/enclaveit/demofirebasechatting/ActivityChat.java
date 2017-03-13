c644fd2049369d57186262bcc5577a1805507bb0
danisluis6 <vuongluis@hotmail.com>
2017-03-13 16:23:20 +0700

    ok

--- app/src/main/java/com/example/enclaveit/demofirebasechatting/ActivityChat.java
+++ app/src/main/java/com/example/enclaveit/demofirebasechatting/ActivityChat.java
@@ -1,57 +1,66 @@
 package com.example.enclaveit.demofirebasechatting;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class ActivityChat extends AppCompatActivity {
 
-    private EditText text;
-    private ListView list;
-    private Button btnchat;
+    private Button btn_send_msg;
+    private EditText input_msg;
+    private TextView chat_conversation;
 
+    private String user_name, room_name,temp_key;
 
-    private String name;
-
-    private ArrayAdapter<String> adapter;
-    private ArrayList<String> listOfMessage;
+    private DatabaseReference root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        initCoponents();
+        /**
+         * We use initCoponents();
+         */
+        btn_send_msg = (Button)this.findViewById(R.id.btn_send_msg);
+        input_msg = (EditText)this.findViewById(R.id.input_msg);
+        chat_conversation = (TextView) this.findViewById(R.id.chat_conversation);
+
 
         /**
-         * Don't understand to do ????
+         * Get intent from parent activity
          */
-        btnchat.setOnClickListener(new View.OnClickListener() {
+        room_name = this.getIntent().getExtras().get("room_name").toString();
+        user_name = this.getIntent().getExtras().get("user_name").toString();
+
+        /**
+         * Set title in room_chat
+         */
+        this.setTitle("Room - "+room_name);
+
+
+        root = FirebaseDatabase.getInstance().getReference().child(room_name);
+        btn_send_msg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Map<String, Object> map = new HashMap<String, Object>();
+                temp_key = root.push().getKey();
+                root.updateChildren(map);
 
-
+                DatabaseReference message_root = root.child(temp_key);
+                Map<String, Object> map2 = new HashMap<String, Object>();
+                map2.put("name",user_name);
+                map2.put("msg",input_msg.getText().toString());
             }
         });
-    }
-
-    private void initCoponents() {
-        text = (EditText)this.findViewById(R.id.text);
-        list = (ListView)this.findViewById(R.id.message);
-        btnchat = (Button)this.findViewById(R.id.btnchat);
     }
 }
